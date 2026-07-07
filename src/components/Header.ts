beb3f6fa2a5b6c24288cf1ec4439900ea1b85b85
wordomni <voltusa.project@gmail.com>
2026-07-07 05:19:58 +0000

    Add Header component

---
+++ src/components/Header.ts
@@ -0,0 +1,12 @@
+export function Header() {
+  return `
+    <header>
+      <h1>TenderPulse v2</h1>
+      <nav>
+        <a href="#">Dashboard</a>
+        <a href="#">Tenders</a>
+        <a href="#">Pricing</a>
+      </nav>
+    </header>
+  `
+}
